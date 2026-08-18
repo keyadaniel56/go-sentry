ea770534dcb1041aa4e00759c0eb45f6368f0075
keyadaniel56 <keyadaniel56@gmail.com>
2026-08-18 12:20:33 +0300

    feat: implemented the claims wrap for the standard jwt registration

--- cmd/api/models/models.go
+++ cmd/api/models/models.go
@@ -1,7 +1,16 @@
 package models
 
+import "github.com/golang-jwt/jwt/v5"
+
 
 type Params[T any] struct{
 	Password string `json:"-"`
 	Data T `json:"data"`
+}
+
+
+//claims wraps the standard JWT registration and embeds the dynamic data type
+type Claims[T any] struct{
+	Data T `json:"data"` // This holds your custom username,emails,etc
+	jwt.RegisteredClaims
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-sentry
 
 go 1.25.0
 
-require golang.org/x/crypto v0.55.0
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	golang.org/x/crypto v0.55.0
+)
